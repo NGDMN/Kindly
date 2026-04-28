9912578929f2188fc7a56ab4b970d1ecf509ef53
Neil Goodman <neilgoodman@live.com>
2026-04-28 09:17:16 -0300

    fix: KindlyApplication.java broken link

--- backend-java/src/main/java/br/com/fiap/kindly/KindlyApplication.java
+++ backend-java/src/main/java/br/com/fiap/kindly/KindlyApplication.java
@@ -3,10 +3,10 @@ package br.com.fiap.kindly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class KindlyApplication {
+
     public static void main(String[] args) {
-        [SpringApplication.run](http://SpringApplication.run)(KindlyApplication.class, args);
+        SpringApplication.run(KindlyApplication.class, args);
     }
 }
