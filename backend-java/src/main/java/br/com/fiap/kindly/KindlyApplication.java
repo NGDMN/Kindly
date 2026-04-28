2253c889bb2aee5673d9f60aaf412c07621699fb
Neil Goodman <neilgoodman@live.com>
2026-04-28 09:03:09 -0300

    chore: add KindlyApplication.java to backend-java

---
+++ backend-java/src/main/java/br/com/fiap/kindly/KindlyApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.kindly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KindlyApplication {
+    public static void main(String[] args) {
+        [SpringApplication.run](http://SpringApplication.run)(KindlyApplication.class, args);
+    }
+}
