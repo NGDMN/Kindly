aaafbdaa1766aefce705a26888ad50d93794b7a7
Neil Goodman <neilgoodman@live.com>
2026-05-05 18:49:04 -0300

    feat: create DataSourceConfig

---
+++ backend-java/src/main/java/br/com/fiap/kindly/config/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package br.com.fiap.kindly.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class DataSourceConfig {
+
+    private final Dotenv dotenv = Dotenv.load();
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("oracle.jdbc.OracleDriver");
+        ds.setUrl(dotenv.get("DB_URL"));
+        ds.setUsername(dotenv.get("DB_USER"));
+        ds.setPassword(dotenv.get("DB_PASSWORD"));
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
